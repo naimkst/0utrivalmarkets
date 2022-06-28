743865fc67763e2bcae5db7b08cc257bcd58723c
Naim Hossain <najmulspark@gmail.com>
2022-06-28 23:07:09 +0600

    Herder and footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,7 +1,158 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import footerLogo from "../../public/assets/images/footer-logo.png";
+import {
+  AiOutlineBehance,
+  AiOutlineInstagram,
+  AiOutlineDribbble,
+  AiOutlineTwitter,
+} from "react-icons/ai";
+import { BsTelephone } from "react-icons/bs";
+import { BiEnvelope } from "react-icons/bi";
 
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <>
+      <div className="bg-[#090D48] py-[65px] items-center">
+        <div className="container max-w-[1307px] m-auto grid grid-cols-1 gap-4 justify-between md:grid-cols-4">
+          <div>
+            <div className="flex justify-center content-center md:content-start">
+              <Image className="m-auto content-center" src={footerLogo} />
+            </div>
+            <div>
+              <ul className="flex justify-center items-center gap-[20px] my-[25px] md:items-start">
+                <li className="bg-[#fff] w-[32px] h-[32px] items-center justify-center flex rounded-full">
+                  <AiOutlineBehance color="#2674D1" />
+                </li>
+                <li className="bg-[#fff] w-[32px] h-[32px] items-center justify-center flex rounded-full">
+                  <AiOutlineDribbble color="#DE778C" />
+                </li>
+                <li className="bg-[#fff] w-[32px] h-[32px] items-center justify-center flex rounded-full">
+                  <AiOutlineTwitter color="#5F98DD" />
+                </li>
+                <li className="bg-[#fff] w-[32px] h-[32px] items-center justify-center flex rounded-full">
+                  <AiOutlineInstagram color="#FF8B2D" />
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div>
+            <h2 className="h2Title text-center md:text-left">Our Company</h2>
+            <ul className="text-center md:text-left">
+              <li>
+                <a
+                  className="text-white text-[20px] leading-[35px] hover:text-[#FF8B2D] "
+                  href=""
+                >
+                  About Us
+                </a>
+              </li>
+              <li>
+                <a
+                  className="text-white text-[20px] leading-[35px] hover:text-[#FF8B2D] "
+                  href=""
+                >
+                  Service
+                </a>
+              </li>
+              <li>
+                <a
+                  className="text-white text-[20px] leading-[35px] hover:text-[#FF8B2D] "
+                  href=""
+                >
+                  Portfolio
+                </a>
+              </li>
+              <li>
+                <a
+                  className="text-white text-[20px] leading-[35px] hover:text-[#FF8B2D] "
+                  href=""
+                >
+                  Blog
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2 className="h2Title text-center md:text-left">Service</h2>
+            <ul className="content-center text-center md:text-left">
+              <li>
+                <a
+                  className="text-white text-[20px] leading-[35px] hover:text-[#FF8B2D] "
+                  href=""
+                >
+                  Amazon account{" "}
+                </a>
+              </li>
+              <li>
+                <a
+                  className="text-white text-[20px] leading-[35px] hover:text-[#FF8B2D] "
+                  href=""
+                >
+                  Management
+                </a>
+              </li>
+              <li>
+                <a
+                  className="text-white text-[20px] leading-[35px] hover:text-[#FF8B2D] "
+                  href=""
+                >
+                  A+/ EBC content
+                </a>
+              </li>
+              <li>
+                <a
+                  className="text-white text-[20px] leading-[35px] hover:text-[#FF8B2D] "
+                  href=""
+                >
+                  Product content writing
+                </a>
+              </li>
+              <li>
+                <a
+                  className="text-white text-[20px] leading-[35px] hover:text-[#FF8B2D] "
+                  href=""
+                >
+                  Product photography
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2 className="h2Title text-center md:text-left">Our Address</h2>
+            <div className="flex-row text-center md:text-left">
+              <div>
+                <p className="text-white text-[20px] leading-[30px] hover:text-[#FF8B2D]">
+                  5055 North 03th Avenue,Penscola,
+                  <br /> FL 32503, New York
+                </p>
+              </div>
+              <div className="mt-[20px] items-center justify-center flex text-center md:text-left md:justify-start">
+                <BsTelephone color="#fff" className="text-[25px] mr-4" />
+                <a
+                  className="text-white text-[20px] leading-[30px] hover:text-[#FF8B2D]"
+                  href="tel:+1 965 047 658 23"
+                >
+                  +1 965 047 658 23
+                </a>
+              </div>
+
+              <div className="mt-[20px] items-center justify-center flex md:text-left md:justify-start">
+                <BiEnvelope color="#fff" className="text-[25px] mr-4" />
+                <a
+                  className="text-white text-[20px] leading-[30px] hover:text-[#FF8B2D]"
+                  href="tel:+1 965 047 658 23"
+                >
+                  +1 965 047 658 23
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="bg-[#FD8332] py-[25px]">
+        <p className="text-center text-white text-[16px]">2022 | All Right Reserved By <span className="font-bold">Outrival.</span></p>
+      </div>
+    </>
+  );
 }
--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,7 +1,115 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import logo from "../../public/assets/images/logo.png";
+import { HiMenuAlt3 } from "react-icons/hi";
 
 export default function Header() {
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const mobileMenu = () => {
+    setIsOpen(!isOpen);
+  };
   return (
-    <div>Header</div>
-  )
+    <div>
+      <div className="container max-w-[1307px] m-auto">
+        <div className="flex items-center justify-between pt-[8px] px-4">
+          <div className="hidden md:block">
+            <Image src={logo} width={173} height={131} />
+          </div>
+          <div className="md:hidden">
+            <Image src={logo} width={85} height={60} />
+          </div>
+          <div className="hidden md:block">
+            <ul className="flex justify-between items-center gap-16 ">
+              <li>
+                <a
+                  className="text-[24px] font-semibold hover:text-[#FF8B2D]"
+                  href=""
+                >
+                  Service
+                </a>
+              </li>
+              <li>
+                <a
+                  className="text-[24px] font-semibold hover:text-[#FF8B2D]"
+                  href=""
+                >
+                  Portfolio
+                </a>
+              </li>
+              <li>
+                <a
+                  className="text-[24px] font-semibold hover:text-[#FF8B2D]"
+                  href=""
+                >
+                  About
+                </a>
+              </li>
+              <li>
+                <a
+                  className="text-[24px] font-semibold hover:text-[#FF8B2D]"
+                  href=""
+                >
+                  Blog
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div>
+            <button className="buttonBg text-white py-[20px] px-[46px] rounded-[7px] text-[24px] font-bold bg-[linear-gradient(276.17deg, #F45356 0%, #FF9127 104.34%)] hidden md:block">
+              Contact Us
+            </button>
+            <div onClick={() => mobileMenu()} className="md:hidden">
+              <HiMenuAlt3 size={40} />
+            </div>
+          </div>
+        </div>
+      </div>
+      {isOpen && (
+        <div className="relative">
+          <div className="md:hidden bg-black z-50 absolute w-full p-7">
+            <ul className="w-full">
+              <li>
+                <a
+                  className="text-[24px] leading-[40px] text-white font-semibold"
+                  href=""
+                >
+                  Service
+                </a>
+              </li>
+              <li>
+                <a
+                  className="text-[24px] leading-[40px] text-white font-semibold"
+                  href=""
+                >
+                  Portfolio
+                </a>
+              </li>
+              <li>
+                <a
+                  className="text-[24px] leading-[40px] text-white font-semibold"
+                  href=""
+                >
+                  About
+                </a>
+              </li>
+              <li>
+                <a
+                  className="text-[24px] leading-[40px] text-white font-semibold"
+                  href=""
+                >
+                  Blog
+                </a>
+              </li>
+
+              <button className="buttonBg text-white py-[10px] px-[30px] rounded-[7px] text-[16px] mt-5 font-bold">
+                Contact Us
+              </button>
+            </ul>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
