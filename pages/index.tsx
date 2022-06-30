e2111b81b86a109debe1d800650d7beefd99779f
Naim Hossain <najmulspark@gmail.com>
2022-06-30 22:33:41 +0600

    Service section

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import AboutSection from '../components/HomePage/AboutSection'
 import CreativeMind from '../components/HomePage/CreativeMind'
 import HappyClientSection from '../components/HomePage/HappyClientSection'
 import HeroSection from '../components/HomePage/HeroSection'
+import SellerSection from '../components/HomePage/SellerSection'
 import TestimonialSection from '../components/HomePage/TestimonialSection'
 import styles from '../styles/Home.module.css'
 
@@ -16,6 +17,7 @@ const Home: NextPage = () => {
       <TestimonialSection/>
       <CreativeMind/>
       <HappyClientSection/>
+      <SellerSection/>
     </>
   )
 }
