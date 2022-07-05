64bff3c0ba38ab5abc34f977fe78fc75ce93d9f7
Naim Hossain <najmulspark@gmail.com>
2022-07-06 01:34:12 +0600

    package done

---
+++ components/ServicePage/PriceSection.tsx
@@ -0,0 +1,113 @@
+import React from "react";
+
+export default function PriceSection() {
+  return (
+    <>
+      <div className="py-[90px]">
+        <div className="max-w-[1307px] m-auto">
+          <div className="grid grid-cols-4 gap-6 items-center">
+            <div className="pricingBoxShadow rounded-[30px] py-9 group hover:bg-gradient-to-r from-[#FC6E36] to-[#E8505B]">
+              <div className="bg-gradient-to-r from-[#FC6E36] to-[#E8505B] w-[80%] rounded-tr-[40px] rounded-br-[40px] text-center py-[20px] text-white text-[20px] font-bold group-hover:text-[#FC6E36] group-hover:bg-gradient-to-r group-hover:from-white group-hover:to-white">
+                Basic
+              </div>
+              <div className="content-center text-center my-10">
+                <p className="text-[20px] font-black group-hover:text-white">Starting At</p>
+                <h2 className="text-[40px] font-black group-hover:text-white">$10.99/<span className="text-[20px]">m</span></h2>
+              </div>
+
+              <div className="content-center text-center">
+                <ul>
+                  <li className="text-[20px] leading-[30px] mb-4 text-[#090D48] group-hover:text-white">20GB Disk Space</li>
+                  <li className="text-[20px] leading-[30px] mb-4 text-[#090D48] group-hover:text-white">100GB Bandwidth</li>
+                  <li className="text-[20px] leading-[30px] mb-4 text-[#090D48] group-hover:text-white">10 Domains Allowed</li>
+                  <li className="text-[20px] leading-[30px] mb-4 text-[#090D48] group-hover:text-white">No Free Dedicated IP</li>
+                  <li className="text-[20px] leading-[30px] mb-4 text-[#090D48] group-hover:text-white">24/7/365 Support Available</li>
+                  <li className="text-[20px] leading-[30px] mb-4 text-[#090D48] group-hover:text-white">30 Email Accounts</li>
+                </ul>
+              </div>
+
+              <div className="text-center mt-14 -mb-10">
+                <a href="" className="bg-gradient-to-r from-[#FC6E36] to-[#E8505B] text-[16px] text-white py-[20px] px-[35px] font-semibold rounded-[10px]">Get Started</a>
+              </div>
+            </div>
+
+            <div className="pricingBoxShadow rounded-[30px] py-9 group hover:bg-gradient-to-r from-[#FC6E36] to-[#E8505B]">
+              <div className="bg-gradient-to-r from-[#FC6E36] to-[#E8505B] w-[80%] rounded-tr-[40px] rounded-br-[40px] text-center py-[20px] text-white text-[20px] font-bold group-hover:text-[#FC6E36] group-hover:bg-gradient-to-r group-hover:from-white group-hover:to-white">
+              Medium
+              </div>
+              <div className="content-center text-center my-10">
+                <p className="text-[20px] font-black group-hover:text-white">Starting At</p>
+                <h2 className="text-[40px] font-black group-hover:text-white">$20.99/<span className="text-[20px]">m</span></h2>
+              </div>
+
+              <div className="content-center text-center">
+                <ul>
+                  <li className="text-[20px] leading-[30px] mb-4 text-[#090D48] group-hover:text-white">20GB Disk Space</li>
+                  <li className="text-[20px] leading-[30px] mb-4 text-[#090D48] group-hover:text-white">100GB Bandwidth</li>
+                  <li className="text-[20px] leading-[30px] mb-4 text-[#090D48] group-hover:text-white">10 Domains Allowed</li>
+                  <li className="text-[20px] leading-[30px] mb-4 text-[#090D48] group-hover:text-white">No Free Dedicated IP</li>
+                  <li className="text-[20px] leading-[30px] mb-4 text-[#090D48] group-hover:text-white">24/7/365 Support Available</li>
+                  <li className="text-[20px] leading-[30px] mb-4 text-[#090D48] group-hover:text-white">30 Email Accounts</li>
+                </ul>
+              </div>
+
+              <div className="text-center mt-14 -mb-10">
+                <a href="" className="bg-gradient-to-r from-[#FC6E36] to-[#E8505B] text-[16px] text-white py-[20px] px-[35px] font-semibold rounded-[10px]">Get Started</a>
+              </div>
+            </div>
+
+            <div className="pricingBoxShadow rounded-[30px] py-9 group hover:bg-gradient-to-r from-[#FC6E36] to-[#E8505B]">
+              <div className="bg-gradient-to-r from-[#FC6E36] to-[#E8505B] w-[80%] rounded-tr-[40px] rounded-br-[40px] text-center py-[20px] text-white text-[20px] font-bold group-hover:text-[#FC6E36] group-hover:bg-gradient-to-r group-hover:from-white group-hover:to-white">
+              Standard
+              </div>
+              <div className="content-center text-center my-10">
+                <p className="text-[20px] font-black group-hover:text-white">Starting At</p>
+                <h2 className="text-[40px] font-black group-hover:text-white">$39.99/<span className="text-[20px]">m</span></h2>
+              </div>
+
+              <div className="content-center text-center">
+                <ul>
+                  <li className="text-[20px] leading-[30px] mb-4 text-[#090D48] group-hover:text-white">20GB Disk Space</li>
+                  <li className="text-[20px] leading-[30px] mb-4 text-[#090D48] group-hover:text-white">100GB Bandwidth</li>
+                  <li className="text-[20px] leading-[30px] mb-4 text-[#090D48] group-hover:text-white">10 Domains Allowed</li>
+                  <li className="text-[20px] leading-[30px] mb-4 text-[#090D48] group-hover:text-white">No Free Dedicated IP</li>
+                  <li className="text-[20px] leading-[30px] mb-4 text-[#090D48] group-hover:text-white">24/7/365 Support Available</li>
+                  <li className="text-[20px] leading-[30px] mb-4 text-[#090D48] group-hover:text-white">30 Email Accounts</li>
+                </ul>
+              </div>
+
+              <div className="text-center mt-14 -mb-10">
+                <a href="" className="bg-gradient-to-r from-[#FC6E36] to-[#E8505B] text-[16px] text-white py-[20px] px-[35px] font-semibold rounded-[10px]">Get Started</a>
+              </div>
+            </div>
+
+            <div className="pricingBoxShadow rounded-[30px] py-9 group hover:bg-gradient-to-r from-[#FC6E36] to-[#E8505B]">
+              <div className="bg-gradient-to-r from-[#FC6E36] to-[#E8505B] w-[80%] rounded-tr-[40px] rounded-br-[40px] text-center py-[20px] text-white text-[20px] font-bold group-hover:text-[#FC6E36] group-hover:bg-gradient-to-r group-hover:from-white group-hover:to-white">
+              Primium
+              </div>
+              <div className="content-center text-center my-10">
+                <p className="text-[20px] font-black group-hover:text-white">Starting At</p>
+                <h2 className="text-[40px] font-black group-hover:text-white">$49.99/<span className="text-[20px]">m</span></h2>
+              </div>
+
+              <div className="content-center text-center">
+                <ul>
+                  <li className="text-[20px] leading-[30px] mb-4 text-[#090D48] group-hover:text-white">20GB Disk Space</li>
+                  <li className="text-[20px] leading-[30px] mb-4 text-[#090D48] group-hover:text-white">100GB Bandwidth</li>
+                  <li className="text-[20px] leading-[30px] mb-4 text-[#090D48] group-hover:text-white">10 Domains Allowed</li>
+                  <li className="text-[20px] leading-[30px] mb-4 text-[#090D48] group-hover:text-white">No Free Dedicated IP</li>
+                  <li className="text-[20px] leading-[30px] mb-4 text-[#090D48] group-hover:text-white">24/7/365 Support Available</li>
+                  <li className="text-[20px] leading-[30px] mb-4 text-[#090D48] group-hover:text-white">30 Email Accounts</li>
+                </ul>
+              </div>
+
+              <div className="text-center mt-14 -mb-10">
+                <a href="" className="bg-gradient-to-r from-[#FC6E36] to-[#E8505B] text-[16px] text-white py-[20px] px-[35px] font-semibold rounded-[10px]">Get Started</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- pages/service.tsx
+++ pages/service.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import AmazonSeller from "../components/ServicePage/AmazonSeller";
 import HeroSection from "../components/ServicePage/HeroSection";
+import PriceSection from "../components/ServicePage/PriceSection";
 
 export default function Service() {
   return (
     <>
       <HeroSection />
       <AmazonSeller />
+      <PriceSection/>
     </>
   );
 }
